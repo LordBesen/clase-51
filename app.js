4460f609dbff07d05732c0bba23c6c43d6606dc9
Federico Besenzoni <besen@hotmail.com>
2021-06-04 22:09:33 -0300

    clase 51

---
+++ app.js
@@ -0,0 +1,40 @@
+require('colors')
+
+const {inquirerMenu,pausa} = require('./helpers/inquirer');
+const Tarea =  require('./models/Tarea');
+const Tareas =  require('./models/Tareas')
+
+
+
+
+const main = async() => {
+    
+    let opt = '';
+   
+    do {
+  //opt = await  inquirerMenu();
+ // console.log({opt});
+ const tareas = new Tareas();
+ const tarea = new Tarea('comida');
+
+ tareas._listado[tarea.id] = tarea;
+ console.log(tareas);
+  
+  
+        await pausa();
+
+
+    }while(opt !== '0');
+    
+   
+
+  
+
+
+
+
+
+
+}
+
+main();
